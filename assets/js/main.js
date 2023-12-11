eba1bf03f69dfc3713ccb77210b652e21832fdb7
acengSI09 <rizqirifah.a@gmail.com>
2023-12-11 21:24:43 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,26 @@ $(document).ready(function() {
         nav: true,
         items: 1,
         navText: [
-            "<li class='fas fa-angle-left'><i>",
-            "<li class='fas fa-angle-right'><i>"
+            "<i class='fas fa-angle-left'></i>",
+            "<i class='fas fa-angle-right'></i>"
         ],
         navContainer: "#slider-hero-nav",
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        itemElement:true,
+    });
+
+    $('#tenaga-pendidik-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        items: 3,
+        // dots: false,
+       
+        navText: [
+            '<i class="fa fa-angle-left"><i>',
+            '<i class="fa fa-angle-right"><i>'
+        ],
         autoplay:true,
         autoplayTimeout:3000,
         autoplayHoverPause:true,
